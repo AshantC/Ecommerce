ab7d145d78923bf17fa637847d5fcc95fe443a0f
AshantC <ashantchaudhary22@gmail.com>
2024-07-15 07:51:22 +0545

    product details

--- home/urls.py
+++ home/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from home.views import HomeView, CategoryItemView, ItemSearchVeiw
+from home.views import HomeView, CategoryItemView, ItemSearchVeiw, ItemDetailView, signup
 
 app_name = "home"
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
     path("category/<slug>", CategoryItemView.as_view(), name="category"),
-    path("search", ItemSearchVeiw.as_view(), name="search")
+    path("item_detail/<slug>", ItemDetailView.as_view(), name="item_detail"),
+    path("search", ItemSearchVeiw.as_view(), name="search"),
+    path("signup", signup, name="signup")
     
 ]
--- home/views.py
+++ home/views.py
@@ -1,3 +1,5 @@
+from django.contrib import messages
+from django.contrib.auth.models import User
 from django.shortcuts import redirect, render
 from django.views import View
 from home.models import Category, Ad, Brand, Item, Slider
@@ -34,5 +36,43 @@ class ItemSearchVeiw(BaseView):
         if search is None:
             return redirect("/")
         else:
-            return render(request, "category.html", self.views)
+            self.views['search_item'] = Item.objects.filter(name__icontains=search)
+            return render(request, "search.html", self.views)
         return render(request, "category.html", self.views)
+    
+class ItemDetailView(BaseView):
+    def get(self, request, slug):
+        self.views['item_details'] = Item.objects.filter(slug=slug)
+        return render(request, "product-detail.html", self.views)
+    
+    
+def signup(request):
+    if request.method =="POST":
+        f_name = request.POST["first_name"]
+        l_name = request.POST["last_name"]
+        email = request.POST["email"]
+        username = request.POST["username"]
+        password = request.POST["password"]
+        c_password = request.POST["cpassword"]
+        if password == c_password:
+            if User.objects.filter(username=username).exists:
+                messages.error(request, "This username is already taken.")
+                return redirect("home:signup")
+            elif User.objects.filter(email=email).exists:
+                messages.error(request, "This email is already taken.")
+                return redirect("home:singup")
+            else: 
+                user = User.objects.create_user(
+                    username = username,
+                    email = email,
+                    password = password,
+                    first_name = f_name,
+                    last_name = l_name
+                )
+                user.save()
+                messages.success(request, "You are successfully registered.")
+                return redirect("home:signup")
+        else:
+            messages.error(request, "Password Not Matched.")
+            return redirect("home:signup")
+    return render(request, "signup.html")
